c60d0a566f4912740e43701da5bc0de7a52bd9c9
Marta Pettersson <marta.pettersson@gmail.com>
2021-01-13 14:51:32 +0100

    Fetch info about the user in CustomerListPage

--- src/pages/CustomerListPage.jsx
+++ src/pages/CustomerListPage.jsx
@@ -3,9 +3,11 @@ import CustomerListItem from "../components/CustomerListItem";
 
 export default function CustomerListPage() {
 	const [customerList, setCustomerList] = useState([]);
+	const [user, setUser] = useState({});
 
 	useEffect(() => {
 		getCustomerList();
+		getUser();
 	}, []);
 
 	//Get Customer List from the backend, since we have a verified token when we login
@@ -23,11 +25,30 @@ export default function CustomerListPage() {
 			.then((data) => setCustomerList(data.results));
 	};
 
-	console.log(customerList);
+	// Retrieve data from the backend about the user
+	const getUser = () => {
+		const url = "https://frebi.willandskill.eu/api/v1/me/";
+		const token = localStorage.getItem("MARTA_WEBB20");
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => setUser(data));
+	};
 
 	return (
 		<div>
 			<h2>Home</h2>
+			<div className="border">
+				<p>
+					{user.firstName} {user.lastName}
+				</p>
+				<p>{user.email}</p>
+			</div>
 			<h3>Customer List</h3>
 			<ul>
 				{customerList.map((item) => {
