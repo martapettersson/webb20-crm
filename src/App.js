b11a0126cfbffe7ab71361fd737039e10b6a7b30
Marta Pettersson <marta.pettersson@gmail.com>
2021-01-13 12:26:44 +0100

    Set up LoginPage with login-form, Fetch token from API and add to Local Storage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
 	return (
 		<div className="container">
 			<h1>CRM</h1>
+			<Switch>
+				<Route path="/login">
+					<LoginPage />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function LoginPage() {
+	const [formData, setFormData] = useState({
+		email: "Marta.Pettersson@yh.nackademin.se",
+		password: "reactjsrules",
+	});
+	const history = useHistory();
+
+	const handleOnChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		const url = "https://frebi.willandskill.eu/api-token-auth/";
+		const payload = {
+			email: formData.email,
+			password: formData.password,
+		};
+		fetch(url, {
+			method: "POST",
+			body: JSON.stringify(payload),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				localStorage.setItem("MARTA_WEBB20", data.token);
+			});
+	};
+	return (
+		<div>
+			<form onSubmit={handleOnSubmit}>
+				<label>Email</label>
+				<input
+					name="email"
+					value={formData.email}
+					onChange={handleOnChange}
+					type="text"
+				/>
+				<label>Password</label>
+				<input
+					name="password"
+					value={formData.password}
+					onChange={handleOnChange}
+					type="text"
+				/>
+				<button type="submit">Log in</button>
+			</form>
+		</div>
+	);
+}
